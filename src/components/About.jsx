e575675880e520464c6a86433582de60e3f7307b
surajit-tunga <surajittunga2005@gmail.com>
2025-08-06 15:23:03 +0530

    Added auto scolling to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { aboutData } from '../constant/index';
 
-const About = ({ onDone }) => {
+const About = ({ onDone, scrollRef }) => {
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
   const intro = aboutData.intro;
+
+  const endRef = useRef(null);
 
   useEffect(() => {
     if (index < intro.length) {
       const timeout = setTimeout(() => {
         setText((prev) => prev + intro[index]);
-        setIndex(index + 1);
+        setIndex((prev) => prev + 1);
       }, 20);
       return () => clearTimeout(timeout);
     } else {
-      if (onDone) onDone(); // Notify parent when done
+      if (onDone) onDone();
     }
   }, [index, intro, onDone]);
 
+  useEffect(() => {
+    if (endRef.current) {
+      endRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }
+  }, [text]);
+
   return (
-    <div className="mx-3 my-1 ">
+    <div className="mx-3 my-5 ">
       <h2 className="text-2xl sm:text-3xl font-bold mb-4 text-yellow-400">About Me</h2>
       <p className="text-base text-white sm:text-lg leading-relaxed whitespace-pre-wrap">
-          <span  className="text-green-600">~$ </span>{text}</p>
+        <span className="text-green-600">~$ </span>
+        {text}
+        <span ref={endRef} className="inline-block w-px h-px align-top pb-10" />
+      </p>
     </div>
   );
 };
